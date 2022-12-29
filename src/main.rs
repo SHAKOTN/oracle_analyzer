fffa2e09afaf824d30c58c2a5c9ea2939d5d56a5
SHAKOTN <shakotn@protonmail.com>
2022-12-29 15:36:16 +0200

    feat: adding new future to handle getting previous round data

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 #[macro_use]
 extern crate log;
 
-use std::error::Error;
-
 use csv::Writer;
 use dotenv::dotenv;
+use std::error::Error as StdError;
 use tokio::task;
-use web3::contract::{Contract, Options};
+use web3::contract::{Contract, Error, Options};
+use web3::transports::Http;
 use web3::types::{Address, U256};
 
 mod constants;
 mod helpers;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() -> Result<(), Box<dyn StdError>> {
     dotenv().ok();
     env_logger::init();
     let web3 = helpers::get_web3(std::env::var("ETHNODEURL").expect("ETHNODEURL must be set."));
@@ -37,15 +37,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
             oracle_address,
             include_bytes!("./res/AggregatorCL.json"),
         )
+        .unwrap();
+        // Get latest round data
+        let round_data: (U256, U256, U256, U256, U256) = contract
+            .query("latestRoundData", (), None, Options::default(), None)
+            .await
             .unwrap();
-        // Fetch the data from the Oracle
-        let future = async move {
-            return contract
-                .query("latestRoundData", (), None, Options::default(), None)
-                .await
-                .unwrap();
-        };
-        let round_data: (U256, U256, U256, U256, U256) = task::spawn(future).await?;
         // Write latest round data to .csv file
         writer.write_record(&[
             round_data.0.to_string(),
@@ -55,6 +52,39 @@ async fn main() -> Result<(), Box<dyn Error>> {
             round_data.4.to_string(),
         ])?;
         writer.flush()?;
+        // TODO: Get previous round data
+        let latest_round = U256::from(round_data.0 - 1);
+        let prev_round_data = task::spawn(get_round_data(latest_round, contract))
+            .await?
+            .unwrap();
+        writer.write_record(&[
+            prev_round_data.0.to_string(),
+            prev_round_data.1.to_string(),
+            prev_round_data.2.to_string(),
+            prev_round_data.3.to_string(),
+            prev_round_data.4.to_string(),
+        ])?;
+        writer.flush()?;
     }
     Ok(())
+}
+
+/// Async get round data by number
+async fn get_round_data(
+    round_number: U256,
+    contract: Contract<Http>,
+) -> Result<(U256, U256, U256, U256, U256), Error> {
+    let round_data: Result<(U256, U256, U256, U256, U256), Error> = contract
+        .query("getRoundData", round_number, None, Options::default(), None)
+        .await;
+    match round_data {
+        Ok(round_data) => {
+            info!("Fetched round data!");
+            Ok(round_data)
+        }
+        Err(error) => {
+            error!("Failed to fetch round data!");
+            Err(error)
+        }
+    }
 }
